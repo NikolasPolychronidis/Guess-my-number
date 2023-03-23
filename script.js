b970113a00aca8123cd063824d2b9c377fd88bb1
NikolasPolychronidis <57706189+NikolasPolychronidis@users.noreply.github.com>
2023-03-23 11:36:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 100;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+let displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.score').textContent = score;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    displayMessage('NO NUMBER');
+  } else if (guess === secretNumber) {
+    displayMessage('YOU WON');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = highscore;
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      score--;
+      displayMessage(
+        guess < secretNumber ? 'Too low try again' : 'Too high try again'
+      );
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('Sorry you have lost');
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ff0000';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+});
